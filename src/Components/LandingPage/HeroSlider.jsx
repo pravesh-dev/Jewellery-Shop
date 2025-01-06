fba58e7244397dd315df2c522cc45b6aa82f5c31
pravesh dav <humantorch1932@gmail.com>
2025-01-06 15:31:29 +0530

    created slider base for hero

--- src/Components/LandingPage/HeroSlider.jsx
+++ src/Components/LandingPage/HeroSlider.jsx
@@ -6,9 +6,24 @@ import img2 from "../../Assets/LandingPage/Hero/img2.jpg";
 import img3 from "../../Assets/LandingPage/Hero/img3.jpg";
 
 function HeroSlider() {
+  const images = [img1, img2, img3];
   return (
     <div className="w-full h-80 border border-red-700 lg:h-[25rem] relative flex flex-col justify-between overflow-hidden">
-      <div className="w-full h-full bg-red-700">
+      <div className="w-full h-[80%] flex justify-center items-center">
+        <div className="w-[80%] h-full bg-red-700 flex items-center gap-48 justify-center">
+          {images.map((image, idx) => {
+            return (
+              <div className="w-80 h-52 rounded-md overflow-hidden">
+              <img
+                src={image}
+                className="w-full h-full object-cover"
+                loading="lazy"
+                alt="image"
+                />
+                </div>
+            );
+          })}
+        </div>
       </div>
       <div className="hidden relative">
         <img src={bottomLine} className="w-full" alt="bottom line image" />
