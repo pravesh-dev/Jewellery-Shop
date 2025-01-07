4bd9012accba4dfc08148d653d3f726784b15e61
pravesh dav <humantorch1932@gmail.com>
2025-01-07 20:58:15 +0530

    designed completely the price slider

--- src/Components/ShopPage/PriceRangeSelector.jsx
+++ src/Components/ShopPage/PriceRangeSelector.jsx
@@ -1,38 +1,52 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
 
 function valuetext(value) {
-  return `${value}°C`;
+  return `${value}`;
 }
 
 export default function PriceRangeSelector() {
-  const [value, setValue] = React.useState([20, 37]);
+  const [value, setValue] = React.useState([20, 100]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    console.log(value);
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Slider
-        getAriaLabel={() => 'Temperature range'}
-        value={value}
-        onChange={handleChange}
-        valueLabelDisplay="auto"
-        getAriaValueText={valuetext}
-        sx={{
-          '& .MuiSlider-track': {
-            backgroundColor: '#006400',
-          },
-          '& .MuiSlider-rail': {
-            backgroundColor: '#868686',
-          },
-          '& .MuiSlider-thumb': {
-            backgroundColor: '#D4AF37',
-          },
-        }}
-      />
-    </Box>
+    <div className="w-full px-7">
+      <Box sx={{ width: "100%" }}>
+        <Slider
+          getAriaLabel={() => "Price range"}
+          value={value}
+          onChange={handleChange}
+          min={20}
+          max={200}
+          getAriaValueText={valuetext}
+          sx={{
+            "& .MuiSlider-track": {
+              backgroundColor: "#006400",
+            },
+            "& .MuiSlider-rail": {
+              backgroundColor: "#868686",
+            },
+            "& .MuiSlider-thumb": {
+              backgroundColor: "#D4AF37",
+            },
+          }}
+        />
+      </Box>
+      <div className="w-full flex justify-between items-center">
+        <div className="w-[45%]">
+            <h2 className="text-xs">From</h2>
+            <span className="border border-dark text-xs px-5 py-1 rounded-full">$ {value[0]}</span>
+        </div>
+        <div className="w-[45%]">
+            <h2 className="text-xs">To</h2>
+            <span className="border border-dark text-xs px-5 py-1 rounded-full">$ {value[1]}</span>
+        </div>
+      </div>
+    </div>
   );
 }
