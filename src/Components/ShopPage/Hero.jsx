d7ace2be583540ad98f58b69ee516b92e9f061c7
pravesh dav <humantorch1932@gmail.com>
2025-01-08 21:51:20 +0530

    done changing data in interval in hero of shop page

--- src/Components/ShopPage/Hero.jsx
+++ src/Components/ShopPage/Hero.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import bgImage1 from '../../Assets/ShopPage/bg-image.jpg';
 import manImage1 from '../../Assets/ShopPage/man.png';
 
 function Hero() {
-  const [data, setData] = useState([
+  const data = [
     {
       bgImage: bgImage1,
       manImage: manImage1,
@@ -28,33 +28,45 @@ function Hero() {
       heading: `SPRING INTO FASHION`,
       para: `Volutpat accumsan suspendisse malesuada lacinia feugiat. Justo fermentum sapien malesuada fringilla facilisis augue.`
     }
-  ]);
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setData(data.map((item, index) => {
-        const newIndex = (index + 1) % data.length;
-        return data[newIndex];
-      }));
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
     }, 2000);
-    return () => clearInterval(interval);
-  }, []);
+
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, [data.length]);
 
   return (
     <section className='w-full px-2 pt-40 pb-5 sm:pb-20 lg:pt-56 xl:px-16'>
       <div className='w-full h-[30rem] relative rounded-xl sm:mx-auto sm:w-[37rem] sm:h-[15rem] md:w-[45rem] lg:w-[65rem] lg:h-[20rem] lg:rounded-[2.4rem] xl:w-full xl:h-[25rem]'>
-        <img src={data[0].bgImage} className='w-full h-full object-cover rounded-xl lg:rounded-[2.4rem]' loading='lazy' alt="bg image" />
+        <img
+          src={data[currentIndex].bgImage}
+          className='w-full h-full object-cover rounded-xl lg:rounded-[2.4rem]'
+          loading='lazy'
+          alt="bg image"
+        />
         <div className='w-full h-full bg-black/20 rounded-xl absolute top-0 left-0 flex flex-col items-center gap-7 sm:flex-row-reverse sm:gap-0 sm:bg-transparent xl:flex-row'>
-          <div className='-mt-16 border-b-2 border-dark w-full flex justify-center sm:w-auto sm:border-none sm:-mt-6 md:-mt-[4.3rem] lg:-mt-[6.8rem] xl:absolute bottom-0 right-0'><img src={data[0].manImage} className='w-60 sm:w-[45rem] xl:w-[30rem]' alt="people image" /></div>
-          <div className='font-bellefair p-3 md:p-7 xl:px-24 xl:w-[55rem]'>
-            <h1 className='text-primary text-3xl md:mb-4 lg:text-[2.9rem] lg:leading-[3.3rem] xl:text-[3.2rem] xl:mb-8'>{data[0].heading}</h1>
-            <p className='lg:text-lg xl:text-xl'>{data[0].para}</p>
+          <div className='-mt-16 border-b-2 border-dark w-full flex justify-center sm:w-auto sm:border-none sm:-mt-6 md:-mt-[4.3rem] lg:-mt-[6.8rem] xl:absolute bottom-0 right-0'>
+            <img
+              src={data[currentIndex].manImage}
+              className='w-60 sm:w-[45rem] xl:w-[30rem]'
+              alt="people image"
+            />
           </div>
-          <div></div>
+          <div className='font-bellefair p-3 md:p-7 xl:px-24 xl:w-[55rem]'>
+            <h1 className='text-primary text-3xl md:mb-4 lg:text-[2.9rem] lg:leading-[3.3rem] xl:text-[3.2rem] xl:mb-8'>
+              {data[currentIndex].heading}
+            </h1>
+            <p className='lg:text-lg xl:text-xl'>{data[currentIndex].para}</p>
+          </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
