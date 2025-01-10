08d65c6cda40db1f1dfedda7e3e6f67a16a5d488
pravesh dav <humantorch1932@gmail.com>
2025-01-10 17:14:27 +0530

    created login signup page background

--- src/Components/LoginSignupPages/Background.jsx
+++ src/Components/LoginSignupPages/Background.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Background() {
   return (
-    <div className='w-full h-full bg-red-600'>
-      
+    <div className='w-full h-full relative overflow-hidden'>
+      <div className='sm:w-[70rem] sm:h-[70rem] rounded-full bg-[#C8F4BF] absolute top-1/2 -translate-y-1/2 left-full -translate-x-1/2'></div>
+      <div className='sm:w-[60rem] sm:h-[60rem] rounded-full bg-[#8FE6A0] absolute top-1/2 -translate-y-1/2 left-full -translate-x-1/2'></div>
+      <div className='sm:w-[50rem] sm:h-[50rem] rounded-full bg-[#62DC7C] absolute top-1/2 -translate-y-1/2 left-full -translate-x-1/2'></div>
     </div>
   )
 }
