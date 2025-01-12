abf528f29cb06c19dea10f63d65f800e54ebe067
pravesh dav <humantorch1932@gmail.com>
2025-01-12 16:15:57 +0530

    created blog data

---
+++ src/Components/BlogPage/BlogsData.js
@@ -0,0 +1,42 @@
+import thumbnail from '../../Assets/Blog/thumbnail.jpg';
+
+const blogsData = [
+    {
+        id: 1,
+        thumbnail: thumbnail,
+        publishData: `October 20, 2009`,
+        title: `Lorem ipsum dolor sit amet consectetur. Mattis praesent imperdiet turpis.`,
+    },
+    {
+        id: 2,
+        thumbnail: thumbnail,
+        publishData: `January 15, 2012`,
+        title: `Curabitur blandit tempus porttitor. Fusce dapibus tellus ac cursus.`,
+    },
+    {
+        id: 3,
+        thumbnail: thumbnail,
+        publishData: `March 10, 2015`,
+        title: `Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus.`,
+    },
+    {
+        id: 4,
+        thumbnail: thumbnail,
+        publishData: `July 25, 2018`,
+        title: `Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero.`,
+    },
+    {
+        id: 5,
+        thumbnail: thumbnail,
+        publishData: `November 5, 2020`,
+        title: `Duis mollis, est non commodo luctus, nisi erat porttitor ligula.`,
+    },
+    {
+        id: 6,
+        thumbnail: thumbnail,
+        publishData: `August 14, 2023`,
+        title: `Morbi leo risus, porta ac consectetur ac, vestibulum at eros.`,
+    },
+];
+
+export { blogsData };
--- src/Pages/BlogLandingPage.jsx
+++ src/Pages/BlogLandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/BlogPage/Hero'
+import Blogs from '../Components/BlogPage/Blogs'
 
 function BlogLandingPage() {
   return (
     <div>
       <Hero />
+      <Blogs />
     </div>
   )
 }
