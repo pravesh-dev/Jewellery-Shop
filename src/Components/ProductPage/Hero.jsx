2a1567ef821a1176bb3d162ed68ee11f070463fe
pravesh dav <humantorch1932@gmail.com>
2025-01-15 15:23:15 +0530

    fixed product info sizes in product page for lg devies

--- src/Components/ProductPage/Hero.jsx
+++ src/Components/ProductPage/Hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ImagesSection from './ImagesSection';
 import ProductInfo from './ProductInfo';
+import { Link } from 'react-router-dom';
 
 function Hero({ product }) {
   return (
     <section className="pt-20 px-2 pb-5 w-80 mx-auto md:w-max lg:pt-28 xl:px-16">
-      <p className="text-sm py-4">
-        Shop &gt; {product.type} &gt; {product.title}
+      <p className="text-sm py-4 text-black/70">
+        <Link to='/shop' className='text-black cursor-pointer'>Shop</Link> &gt; {product.type} &gt; {product.title}
       </p>
-      <div className='md:flex justify-center gap-20 lg:gap-10 xl:gap-20'>
+      <div className='md:flex justify-center gap-20 lg:gap-24 lg:w-[50rem] lg:justify-between bg-red-500 xl:gap-20'>
         <ImagesSection product={product} />
         <ProductInfo product={product} />
       </div>
--- src/Components/ProductPage/ProductInfo.jsx
+++ src/Components/ProductPage/ProductInfo.jsx
@@ -14,7 +14,7 @@ function ProductInfo({ product }) {
       stars.push(
         <IoStarSharp
           key={i}
-          className={`${i <= rating ? "text-secondary" : "text-gray-300"} lg:text-3xl xl:text-4xl`}
+          className={`${i <= rating ? "text-secondary" : "text-gray-300"} lg:text-2xl xl:text-4xl`}
         />
       );
     }
@@ -22,18 +22,18 @@ function ProductInfo({ product }) {
   };
 
   return (
-    <div className="w-full font-bellefair py-4 md:w-80 md:py-0 lg:w-[27rem] xl:w-[35rem]">
-      <h1 className="text-3xl lg:text-4xl xl:text-5xl">{product.name}</h1>
-      <div className="flex gap-2 items-center lg:my-2 xl:my-4">
+    <div className="w-full font-bellefair py-4 md:w-80 md:py-0 lg:w-[22rem] xl:w-[35rem]">
+      <h1 className="text-3xl xl:text-5xl">{product.name}</h1>
+      <div className="flex gap-2 items-center lg:my-1 xl:my-4">
         <div className="flex gap-1 items-center text-lg">
           {renderStars(Math.round(product.reviews.averageRating))}
         </div>
-        <p className="text-sm font-lora lg:text-lg xl:text-xl">{product.reviews.totalReviews} reviews</p>
+        <p className="text-sm font-lora lg:text-base xl:text-xl">{product.reviews.totalReviews} reviews</p>
       </div>
-      <h2 className="text-black text-xl my-3 lg:text-3xl xl:text-[2.5rem] xl:my-8">
+      <h2 className="text-black text-xl my-3 lg:text-2xl xl:text-[2.5rem] xl:my-8">
         {product.onSale ? (
           <div className="flex gap-3">
-            <span className="text-black/50 line-through text-[1rem] lg:text-[1.5rem] xl:text-[2rem]">
+            <span className="text-black/50 line-through text-[1rem] lg:text-[1.1rem] xl:text-[2rem]">
               $ {product.price}
             </span>
             <span>
@@ -48,29 +48,29 @@ function ProductInfo({ product }) {
           <span>$ {product.price}</span>
         )}
       </h2>
-      <p className="lg:text-2xl lg:mb-10 xl:text-4xl xl:mb-12">{product.description}</p>
+      <p className="lg:text-xl lg:mb-10 xl:text-4xl xl:mb-12">{product.description}</p>
       <div className="flex my-3">
         <div className="w-1/4 flex flex-col items-center gap-1">
-            <span className="text-xl bg-secondary text-white p-2 rounded-full xl:text-3xl"><TbTruckDelivery /></span>
-            <h3 className="font-lora text-xs text-center lg:text-base xl:text-xl">Free Delivery</h3>
+            <span className="text-xl bg-secondary text-white p-2 rounded-full lg:text-base xl:text-3xl"><TbTruckDelivery /></span>
+            <h3 className="font-lora text-xs text-center xl:text-xl">Free Delivery</h3>
         </div>
         <div className="w-1/4 flex flex-col items-center gap-1">
-            <span className="text-xl bg-secondary text-white p-2 rounded-full xl:text-3xl"><SlLock /></span>
-            <h3 className="font-lora text-xs text-center lg:text-base xl:text-xl">Secure Transaction</h3>
+            <span className="text-xl bg-secondary text-white p-2 rounded-full lg:text-base xl:text-3xl"><SlLock /></span>
+            <h3 className="font-lora text-xs text-center xl:text-xl">Secure Transaction</h3>
         </div>
         <div className="w-1/4 flex flex-col items-center gap-1">
-            <span className="text-xl bg-secondary text-white p-2 rounded-full xl:text-3xl"><AiFillGolden /></span>
-            <h3 className="font-lora text-xs text-center lg:text-base xl:text-xl">24 Carat
+            <span className="text-xl bg-secondary text-white p-2 rounded-full lg:text-base xl:text-3xl"><AiFillGolden /></span>
+            <h3 className="font-lora text-xs text-center xl:text-xl">24 Carat
             Gold</h3>
         </div>
         <div className="w-1/4 flex flex-col items-center gap-1">
-            <span className="text-xl bg-secondary text-white p-2 rounded-full xl:text-3xl"><IoTrophySharp /></span>
-            <h3 className="font-lora text-xs text-center lg:text-base xl:text-xl">Top Brand</h3>
+            <span className="text-xl bg-secondary text-white p-2 rounded-full lg:text-base xl:text-3xl"><IoTrophySharp /></span>
+            <h3 className="font-lora text-xs text-center xl:text-xl">Top Brand</h3>
         </div>
       </div>
-      <div className="h-10 flex gap-4 lg:mt-8 lg:h-14 xl:h-20">
-        <button className="w-[80%] h-full capitalize rounded-sm text-xl text-center bg-secondary text-white lg:text-3xl lg:rounded-md xl:text-[2.5rem]">Add to cart</button>
-        <button className="w-[20%] h-full rounded-sm text-xl flex justify-center items-center bg-secondary text-white lg:text-3xl lg:rounded-md xl:text-5xl"><IoMdHeart /></button>
+      <div className="h-10 flex gap-4 lg:mt-8 lg:h-11 xl:h-20">
+        <button className="w-[80%] h-full capitalize rounded-sm text-xl text-center bg-secondary text-white lg:text-2xl lg:w-[85%] lg:rounded-md xl:text-[2.5rem]">Add to cart</button>
+        <button className="w-[20%] h-full rounded-sm text-xl flex justify-center items-center bg-secondary text-white lg:text-2xl lg:w-[15%] lg:rounded-md xl:text-5xl"><IoMdHeart /></button>
       </div>
     </div>
   );
