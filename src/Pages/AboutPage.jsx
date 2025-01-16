05c4fcb8930e2e28b10a1beae8a13fb47086d3d0
pravesh dav <humantorch1932@gmail.com>
2025-01-16 17:19:28 +0530

    created our team file and added rounting

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../Components/AboutPage/Hero';
 import AboutMore from '../Components/AboutPage/AboutMore';
+import OurTeam from '../Components/AboutPage/OurTeam';
 
 function AboutPage() {
   return (
     <div>
       <Hero />
       <AboutMore />
+      <OurTeam />
     </div>
   )
 }
