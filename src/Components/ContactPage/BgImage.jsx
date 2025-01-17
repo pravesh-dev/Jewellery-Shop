9f155823dfea107950e4a7b91d3d57021d351760
pravesh dav <humantorch1932@gmail.com>
2025-01-17 12:57:40 +0530

    responsive bg image in contact page

--- src/Components/ContactPage/BgImage.jsx
+++ src/Components/ContactPage/BgImage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import contactBg from '../../Assets/contact-bg.jpg';
 function BgImage() {
   return (
-    <div className='px-2 py-10'>
+    <div className='px-2 py-10 md:px-10 lg:px-20 xl:py-20 xl:w-[70rem] xl:px-0 xl:mx-auto'>
       <div className="w-full h-64 rounded-sm lg:h-80 lg:rounded-md mx-auto">
         <img
           src={contactBg}
