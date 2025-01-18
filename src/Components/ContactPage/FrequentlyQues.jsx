fa16c79841eef0b24312efdddc865c956206977d
pravesh dav <humantorch1932@gmail.com>
2025-01-18 17:05:53 +0530

    created faq file

---
+++ src/Components/ContactPage/FrequentlyQues.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function FrequentlyQues() {
+  return (
+    <div className="w-full px-2 font-bellefair py-10">
+      <div>
+        <h2 className="text-primary drop-shadow-[0_0_0.4px_#006400] lg:text-xl">
+          FAQ
+        </h2>
+        <h1 className="text-primary text-3xl drop-shadow-[0_0_0.4px_#006400]">
+          Frequently asked questions.
+        </h1>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default FrequentlyQues;
--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Contact from '../Components/ContactPage/Contact'
 import BgImage from '../Components/ContactPage/BgImage'
+import FrequentlyQues from '../Components/ContactPage/FrequentlyQues'
 
 function ContactPage() {
   return (
     <>
       <Contact />
       <BgImage />
+      <FrequentlyQues />
     </>
   )
 }
