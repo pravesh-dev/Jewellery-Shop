b8a4cdf996d5adb5272ffd4da483963cfd0b5133
pravesh dav <humantorch1932@gmail.com>
2025-01-21 01:30:42 +0530

    size fixes for signup page

--- src/Components/LoginSignupPages/Signup.jsx
+++ src/Components/LoginSignupPages/Signup.jsx
@@ -52,19 +52,19 @@ function Signup() {
   return (
     <div className="w-full h-full pt-10 absolute top-0 left-0 font-mulish flex justify-center md:gap-10 lg:gap-20 xl:px-28 xl:justify-between">
       <form
-        className="w-80 px-2 h-full flex flex-col justify-center lg:w-96 xl:ml-14"
+        className="w-80 px-2 h-full flex flex-col justify-center lg:w-96 xl:ml-14 xl:w-auto"
         onSubmit={handleFormSubmit}
       >
-        <div className="flex justify-between px-3 mb-10 lg:mb-14">
+        <div className="flex justify-between px-3 mb-10 lg:mb-14 xl:justify-start xl:gap-16">
           <Link
             to="/login"
-            className="text-gray-400 text-2xl border-b-2 border-gray-300 px-2 pb-1 lg:text-4xl lg:px-4"
+            className="text-gray-400 text-2xl border-b-2 border-gray-300 px-2 pb-1 lg:text-4xl lg:px-4 xl:text-[2.1rem] text-stroke-1 xl:pb-3 xl:px-6 xl:border-b-[3.5px]"
           >
             Login
           </Link>
           <Link
             to="/signup"
-            className="text-[#62DC7C] text-2xl border-b-2 border-[#62DC7C]  px-2 pb-1 lg:text-4xl lg:px-4"
+            className="text-[#62DC7C] text-2xl border-b-2 border-[#62DC7C] px-2 pb-1 lg:text-4xl lg:px-4 xl:text-[2.1rem] text-stroke-1 xl:pb-3 xl:px-6 xl:border-b-[3.5px]"
           >
             Sign up
           </Link>
@@ -76,11 +76,11 @@ function Signup() {
         )}
 
         {/* Input Fields */}
-        <div className="space-y-8 lg:space-y-12">
-          <div className="relative">
+        <div className="space-y-8 lg:space-y-12 xl:space-y-9">
+          <div className="relative xl:w-[28.6rem] xl:h-[3.6rem]">
             <img
               src={name}
-              className="w-6 absolute left-3 top-1/2 transform -translate-y-1/2"
+              className="w-6 absolute left-3 top-1/2 transform -translate-y-1/2 xl:w-[1.8rem] xl:left-6"
               alt="name icon"
             />
             <input
@@ -90,13 +90,13 @@ function Signup() {
               onChange={handleInputChange}
               placeholder="Full Name"
               autoComplete="off"
-              className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16"
+              className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16 xl:py-0 xl:h-full xl:text-[1.12rem] xl:pl-20"
             />
           </div>
-          <div className="relative">
+          <div className="relative xl:w-[28.6rem] xl:h-[3.6rem]">
             <img
               src={user}
-              className="w-6 absolute left-3 top-1/2 transform -translate-y-1/2"
+              className="w-6 absolute left-3 top-1/2 transform -translate-y-1/2 xl:w-[1.8rem] xl:left-6"
               alt="user icon"
             />
             <input
@@ -106,13 +106,13 @@ function Signup() {
               onChange={handleInputChange}
               placeholder="Email Address"
               autoComplete="off"
-              className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16"
+              className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16 xl:py-0 xl:h-full xl:text-[1.12rem] xl:pl-20"
             />
           </div>
-          <div className="relative">
+          <div className="relative xl:w-[28.6rem] xl:h-[3.6rem]">
             <img
               src={call}
-              className="w-6 absolute left-3 top-1/2 transform -translate-y-1/2"
+              className="w-6 absolute left-3 top-1/2 transform -translate-y-1/2 xl:w-[1.8rem] xl:left-6"
               alt="phone icon"
             />
             <input
@@ -122,15 +122,15 @@ function Signup() {
               onChange={handleInputChange}
               placeholder="Phone Number"
               autoComplete="off"
-              className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16"
+              className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16 xl:py-0 xl:h-full xl:text-[1.12rem] xl:pl-20"
             />
           </div>
 
           {/* Password */}
-          <div className="relative">
+          <div className="relative xl:w-[28.6rem] xl:h-[3.6rem]">
             <img
               src={lock}
-              className="w-6 absolute left-3 top-1/2 transform -translate-y-1/2"
+              className="w-6 absolute left-3 top-1/2 transform -translate-y-1/2 xl:w-[1.8rem] xl:left-6"
               alt="lock icon"
             />
             <input
@@ -139,13 +139,13 @@ function Signup() {
               value={formData.password}
               onChange={handleInputChange}
               placeholder="Password"
-              className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16"
+              className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16 xl:py-0 xl:h-full xl:text-[1.12rem] xl:pl-20"
             />
           </div>
-          <div className="relative">
+          <div className="relative xl:w-[28.6rem] xl:h-[3.6rem]">
             <img
               src={lock}
-              className="w-6 absolute left-3 top-1/2 transform -translate-y-1/2"
+              className="w-6 absolute left-3 top-1/2 transform -translate-y-1/2 xl:w-[1.8rem] xl:left-6"
               alt="lock icon"
             />
             <input
@@ -154,7 +154,7 @@ function Signup() {
               value={formData.confirmPassword}
               onChange={handleInputChange}
               placeholder="Re-Enter Password"
-              className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16"
+              className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16 xl:py-0 xl:h-full xl:text-[1.12rem] xl:pl-20"
             />
           </div>
         </div>
@@ -162,13 +162,13 @@ function Signup() {
         {/* Signup Button */}
         <button
           type="submit"
-          className="w-full mt-6 bg-[#009400] text-accent text-lg py-2 rounded-md lg:w-auto lg:px-10 lg:rounded-full lg:self-end"
+          className="w-full mt-6 bg-[#009400] text-accent text-lg py-2 rounded-md lg:w-auto lg:px-10 lg:rounded-full xl:mt-12 xl:p-0 xl:w-[9.8rem] xl:h-[3.3rem] xl:text-[1.5rem]"
         >
           Signup
         </button>
       </form>
       <div className="hidden md:flex justify-center items-center">
-        <img className="w-80 lg:w-[27rem]" src={sideImage} alt="image" />
+        <img className="w-80 lg:w-[27rem] xl:w-[32rem]" src={sideImage} alt="image" />
       </div>
     </div>
   );
--- src/Pages/SignupPage.jsx
+++ src/Pages/SignupPage.jsx
@@ -4,7 +4,7 @@ import Signup from '../Components/LoginSignupPages/Signup'
 
 function SignupPage() {
   return (
-    <section className='relative w-full h-[42rem] lg:h-[50rem]'>
+    <section className='relative w-full h-[42rem] lg:h-[50rem] xl:h-[55rem]'>
       <Background />
       <Signup />
     </section>
