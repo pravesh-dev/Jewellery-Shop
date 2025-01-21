f13766f8e1fbeef7b4e9d331ed7490b327f109cb
pravesh dav <humantorch1932@gmail.com>
2025-01-21 17:09:21 +0530

    some fixes in othercategories

--- src/Components/ShopPage/OtherCategories.jsx
+++ src/Components/ShopPage/OtherCategories.jsx
@@ -9,24 +9,24 @@ function OtherCategories() {
         Other category
       </h1>
       <div className="flex flex-wrap gap-2 md:gap-10 xl:justify-between xl:gap-0">
-        <div className="w-80 border border-black flex rounded-lg overflow-hidden lg:w-[30rem] lg:h-60 xl:rounded-[2.5rem] xl:w-[49%] xl:min-h-[16rem]">
-            <div className="w-[60%] flex flex-col justify-center items-start gap-1 p-2 lg:p-5 xl:gap-4 xl:px-8 xl:w-[65%]">
-                <h2 className="text-xl lg:text-2xl xl:text-[1.5rem]">Women Jewellery</h2>
-                <p className="text-xs lg:text-sm xl:text-[1.12rem]">Lorem ipsum dolor sit amet consectetur. Sed velit in vulputate amet vulputate donec pretium. Amet adipiscing habitant ac faucibus eu risus a.</p>
-                <button className="bg-secondary text-accent px-4 py-1 rounded-full xl:text-[1.12rem] xl:px-6 xl:w-[8.7rem] xl:h-[2.2rem] xl:py-0">Explore Now</button>
+        <div className="w-80 border border-black flex rounded-lg overflow-hidden lg:w-[30rem] lg:h-60 xl:rounded-[2rem] xl:w-[49%] xl:min-h-[16rem]">
+            <div className="w-[60%] flex flex-col justify-center items-start gap-1 p-2 lg:p-5 xl:gap-4 xl:px-12 xl:w-[65%]">
+                <h2 className="text-xl lg:text-2xl xl:text-[1.5rem] text-stroke-xs">Women Jewellery</h2>
+                <p className="text-xs lg:text-sm xl:text-[1.12rem] xl:leading-5">Lorem ipsum dolor sit amet consectetur. Sed velit in vulputate amet vulputate donec pretium. Amet adipiscing habitant ac faucibus eu risus a.</p>
+                <button className="bg-secondary text-accent px-4 py-1 rounded-full xl:text-[1.12rem] xl:px-6 xl:w-[8.7rem] xl:h-[2.2rem] xl:py-0 xl:mt-5">Explore Now</button>
             </div>
             <div className="w-[40%] xl:w-[35%]">
-                <img src={img1} className="w-full h-full object-cover" alt="women jewellery image" />
+                <img src={img1} className="w-full h-full object-cover" alt="image" />
             </div>
         </div>
-        <div className="w-80 border border-black flex rounded-lg overflow-hidden lg:w-[30rem] lg:h-60 xl:rounded-[2.5rem] xl:w-[49%] xl:min-h-[16rem]">
-            <div className="w-[60%] flex flex-col justify-center items-start gap-1 p-2 lg:p-5 xl:gap-4 xl:px-8 xl:w-[65%]">
-                <h2 className="text-xl lg:text-2xl xl:text-[1.5rem]">Accessories</h2>
-                <p className="text-xs lg:text-sm xl:text-[1.12rem]">Lorem ipsum dolor sit amet consectetur. Sed velit in vulputate amet vulputate donec pretium. Amet adipiscing habitant ac faucibus eu risus a.</p>
-                <button className="bg-secondary text-accent px-4 py-1 rounded-full xl:text-[1.12rem] xl:px-6 xl:w-[8.7rem] xl:h-[2.2rem] xl:py-0">Explore Now</button>
+        <div className="w-80 border border-black flex rounded-lg overflow-hidden lg:w-[30rem] lg:h-60 xl:rounded-[2rem] xl:w-[49%] xl:min-h-[16rem]">
+            <div className="w-[60%] flex flex-col justify-center items-start gap-1 p-2 lg:p-5 xl:gap-4 xl:px-12 xl:w-[65%]">
+                <h2 className="text-xl lg:text-2xl xl:text-[1.5rem] text-stroke-xs">Accessories</h2>
+                <p className="text-xs lg:text-sm xl:text-[1.12rem] xl:leading-5">Lorem ipsum dolor sit amet consectetur. Sed velit in vulputate amet vulputate donec pretium. Amet adipiscing habitant ac faucibus eu risus a.</p>
+                <button className="bg-secondary text-accent px-4 py-1 rounded-full xl:text-[1.12rem] xl:px-6 xl:w-[8.7rem] xl:h-[2.2rem] xl:py-0 xl:mt-5">Explore Now</button>
             </div>
             <div className="w-[40%] xl:w-[35%]">
-                <img src={img2} className="w-full h-full object-cover" alt="women jewellery image" />
+                <img src={img2} className="w-full h-full object-cover" alt="image" />
             </div>
         </div>
       </div>
