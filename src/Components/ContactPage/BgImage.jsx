be3e2ce0eec93aad86c83d40bbedeebf7dbbcf54
pravesh dav <humantorch1932@gmail.com>
2025-01-21 14:15:31 +0530

    added map in contact page

--- src/Components/ContactPage/BgImage.jsx
+++ src/Components/ContactPage/BgImage.jsx
@@ -4,12 +4,7 @@ function BgImage() {
   return (
     <div className='px-2 py-10 md:px-10 lg:px-20 xl:py-20 xl:w-[70rem] xl:px-0 xl:mx-auto'>
       <div className="w-full h-64 rounded-sm lg:h-80 lg:rounded-md mx-auto">
-        <img
-          src={contactBg}
-          className="w-full h-full object-cover rounded-sm lg:rounded-md object-[0%_40%]"
-          loading="lazy"
-          alt="background image related to about us"
-        />
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28035.403603837625!2d77.07949464303401!3d28.556984445157337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1b85fc2a2d89%3A0xbef376182c43ed9d!2sIndira%20Gandhi%20International%20Airport!5e0!3m2!1sen!2sin!4v1737448821150!5m2!1sen!2sin" width="100%" height="100%" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
   )
