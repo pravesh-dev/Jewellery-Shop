06ead54553f7cdcdd340a2fd9edb49fd052b55e5
pravesh dav <humantorch1932@gmail.com>
2025-01-23 16:34:42 +0530

    styled wishlist card for mobile

--- src/Components/WishlistPage/MobileWishlist.jsx
+++ src/Components/WishlistPage/MobileWishlist.jsx
@@ -4,20 +4,20 @@ function MobileWishlist({items}) {
   const [product, setProduct] = useState(items[0]);
   console.log(product)
   return (
-    <div className='w-full'>
-      <div className='flex justify-between'>
+    <div className='w-full min-h-screen mt-3'>
+      <div className='flex gap-3'>
         <div className='w-[7.6rem] h-[6.8rem] overflow-hidden rounded-md'>
           <img src={product.image} className='w-full h-full object-cover' alt="" />
         </div>
-        <div>
-          <h1 className='text-[1rem] font-bellefair'>{product.name}</h1>
-          <h2 className='font-Cormorant text-primary'>In stoke</h2>
-          <div>
+        <div className='flex flex-col'>
+          <h1 className='text-[1rem] font-bellefair leading-4'>{product.name}</h1>
+          <h2 className='font-Cormorant text-primary text-[0.72rem] italic'>In stoke</h2>
+          <div className='flex gap-5'>
             <button className='text-[#A0A0A0] text-[0.75rem] font-bellefair'>Remove</button>
             <button className='text-[#A0A0A0] text-[0.75rem] font-bellefair'>Share</button>
           </div>
-          <div className='flex items-center gap-2'>
-            <span className='font-lora text-[0.7rem] bg-[#FF0000] text-accent py-1 px-4 rounded-[0.23rem]'>{product.onSale ? `${product.discount}% Off` : `N/A`}</span>
+          <div className='flex items-center gap-5 mb-2'>
+            <span className='font-lora text-[0.7rem] bg-[#FF0000] text-accent py-[0.13rem] px-4 rounded-[0.23rem]'>{product.onSale ? `${product.discount}% Off` : `N/A`}</span>
             <h3 className="text-[1rem]">
               {product.onSale ? (
                 <div className="flex flex-col -space-y-1">
@@ -37,7 +37,7 @@ function MobileWishlist({items}) {
               )}
             </h3>
           </div>
-          <button className='text-sm text-accent bg-secondary px-5 py-1 rounded-[5px]'>Add To Cart</button>
+          <button className='text-sm text-accent bg-secondary px-5 py-1 rounded-[5px] w-full'>Add To Cart</button>
         </div>
       </div>
     </div>
