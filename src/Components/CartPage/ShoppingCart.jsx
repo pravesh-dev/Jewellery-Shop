730bc08b39808a6cc45789d3e3d0720c4b88b227
pravesh dav <humantorch1932@gmail.com>
2025-01-24 13:37:45 +0530

    createdd usercartpage file and performed routing

---
+++ src/Components/CartPage/ShoppingCart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ShoppingCart({items}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShoppingCart
--- src/Pages/UserCartPage.jsx
+++ src/Pages/UserCartPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { items } from '../Components/ProductsData';
+import ShoppingCart from '../Components/CartPage/ShoppingCart';
 
 function UserCartPage() {
   return (
     <div>
-      
+      <ShoppingCart items={items} />
     </div>
   )
 }
