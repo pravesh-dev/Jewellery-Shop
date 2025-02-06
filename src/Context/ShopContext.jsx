e24b665b8e9e2b6b5c02d2daa0f33f5d8d968dc6
pravesh dav <humantorch1932@gmail.com>
2025-02-06 14:08:02 +0530

    created shop context for the website

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,23 @@
+import { createContext } from "react";
+import { items } from "../Components/ProductsData";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = ({children}) =>{
+
+    const currency = '£';
+    const stad_delivery_fee = 100;
+    const fast_delivery_fee = 150;
+
+    const value = {
+        items, currency, stad_delivery_fee, fast_delivery_fee
+    }
+
+    return (
+        <ShopContext.Provider>
+            {children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
