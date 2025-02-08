6d21d8af1e8f0871afdb837af4b55a9dc48b761e
pravesh dav <humantorch1932@gmail.com>
2025-02-08 15:56:27 +0530

    s

--- src/Components/WishlistPage/LargeWishlist.jsx
+++ src/Components/WishlistPage/LargeWishlist.jsx
@@ -30,21 +30,21 @@ function LargeWishlist() {
 
             {/* Wishlist Items */}
             {wishlistProducts.map((product) => (
-                <div key={product.id} className='grid grid-cols-12 mb-6 border-t pt-4'>
+                <div key={product.id} className='grid grid-cols-12 mb-6 pt-4'>
                     <div className='col-span-5 flex items-start gap-2 lg:gap-10'>
                         <div className='w-[7.6rem] flex-shrink-0 h-[6.8rem] lg:w-[12rem] lg:h-[10.9rem] rounded-[5px] overflow-hidden'>
                             <img src={product.image} className='w-full h-full object-cover' alt="product thumbnail" loading='lazy' />
                         </div>
-                        <div className='font-bellefair pt-2 lg:pt-4'>
+                        <div className='font-bellefair pt-2 lg:pt-4 xl:pt-10'>
                             <h1 className='text-[1rem] leading-4 lg:leading-7 lg:text-[1.5rem]'>{product.name}</h1>
-                            <div className='flex gap-2 lg:gap-5'>
+                            <div className='flex gap-2 lg:gap-5 mt-4 xl:mt-6'>
                                 <button onClick={() => removeFromWishlist(product.id)} className='text-[#A0A0A0] text-[0.75rem] lg:text-[1.12rem]'>Remove</button>
                                 <button className='text-[#A0A0A0] text-[0.75rem] lg:text-[1.12rem]'>Share</button>
                             </div>
                         </div>
                     </div>
 
-                    <div className='col-span-4 grid grid-cols-3 gap-2 text-center pt-2 lg:pt-4'>
+                    <div className='col-span-4 grid grid-cols-3 gap-2 text-center pt-2 lg:pt-4 xl:pt-10'>
                       <span>
                         <span className='font-lora text-[0.7rem] py-[0.13rem] px-3 rounded-[0.23rem] text-accent bg-[#ff0000] lg:text-[1rem]'>
                             {product.onSale ? `${product.discount}% Off` : `N/A`}
@@ -67,7 +67,7 @@ function LargeWishlist() {
                         <h3 className='font-Cormorant text-primary text-[0.72rem] italic lg:text-[1.12rem]'>In stock</h3>
                     </div>
 
-                    <div className='col-span-3 flex justify-center pt-2 lg:pt-4'>
+                    <div className='col-span-3 flex justify-center items-start pt-2 lg:pt-4 xl:pt-10'>
                         <button onClick={() => addToCart(product.id)} className='text-sm text-accent bg-secondary px-5 py-1 rounded-[5px] lg:text-[1.12rem] lg:p-0 lg:w-[8.5rem] lg:h-[2.5rem]'>
                             Add To Cart
                         </button>
