dd862d6ef0981e49cd90f10679c0ef9ec68dec27
pravesh dav <humantorch1932@gmail.com>
2025-02-10 14:32:01 +0530

    more condition in verify request

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-import React, { useContext, useEffect } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Layout from './Layout'
-import NoPage from './Pages/NoPage'
-import Home from './Pages/Home'
-import Shop from './Pages/Shop'
-import ProductPage from './Pages/ProductPage'
-import ScrollToTop from './Components/ScrollToTop'
-import LoginPage from './Pages/LoginPage'
-import SignupPage from './Pages/SignupPage'
-import BlogLandingPage from './Pages/BlogLandingPage'
-import AboutPage from './Pages/AboutPage'
-import ContactPage from './Pages/ContactPage'
-import WishlistPage from './Pages/WishlistPage'
-import UserCartPage from './Pages/UserCartPage'
-import axios from 'axios'; // Import axios for HTTP requests
-import { AuthContext } from './Context/AuthContext'
+import React, { useContext, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import NoPage from "./Pages/NoPage";
+import Home from "./Pages/Home";
+import Shop from "./Pages/Shop";
+import ProductPage from "./Pages/ProductPage";
+import ScrollToTop from "./Components/ScrollToTop";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+import BlogLandingPage from "./Pages/BlogLandingPage";
+import AboutPage from "./Pages/AboutPage";
+import ContactPage from "./Pages/ContactPage";
+import WishlistPage from "./Pages/WishlistPage";
+import UserCartPage from "./Pages/UserCartPage";
+import axios from "axios"; // Import axios for HTTP requests
+import { AuthContext } from "./Context/AuthContext";
 
 function App() {
   const { login } = useContext(AuthContext);
   useEffect(() => {
     const verifyCredentials = async () => {
       try {
-        const response = await axios.post('https://jewellery.hexadefend.com/Backend/auth/verify.php', { withCredentials: true });
-        login(response.data);
-        console.log('from verify app' ,response.data);
+        const response = await axios.post(
+          "https://jewellery.hexadefend.com/Backend/auth/verify.php",
+          { withCredentials: true }
+        );
+        if (response.data.status === "success") {
+          login(response.data);
+          console.log("success verify app", response.data);
+        } else if (response.data.status === "error") {
+          console.error("Error verifying credentials:", response.data);
+        }
       } catch (error) {
-        console.error('Error verifying credentials:', error);
+        console.error("Error verifying credentials:", error);
       }
     };
 
@@ -34,24 +41,24 @@ function App() {
 
   return (
     <BrowserRouter>
-    <ScrollToTop />
+      <ScrollToTop />
       <Routes>
-        <Route path='/' element={<Layout/>}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path='/shop' element={<Shop/>} />
-          <Route path='/shop/:type/:name/:id' element={<ProductPage/>} />
-          <Route path='/about' element={<AboutPage />} />
-          <Route path='/blog' element={<BlogLandingPage />} />
-          <Route path='/wishlist' element={<WishlistPage />} />
-          <Route path='/user-cart' element={<UserCartPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<SignupPage />} />
-          <Route path='/contact' element={<ContactPage />} />
-          <Route path='*' element={<NoPage/>} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/:type/:name/:id" element={<ProductPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/blog" element={<BlogLandingPage />} />
+          <Route path="/wishlist" element={<WishlistPage />} />
+          <Route path="/user-cart" element={<UserCartPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
