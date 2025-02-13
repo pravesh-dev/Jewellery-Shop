4e8282f6e3261287d6f219028db03e5bef40ef72
pravesh dav <humantorch1932@gmail.com>
2025-02-13 20:41:14 +0530

    added comments in signuppag

--- src/Pages/SignupPage.jsx
+++ src/Pages/SignupPage.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Background from '../Components/LoginSignupPages/Background'
-import Signup from '../Components/LoginSignupPages/Signup'
+import React from 'react' // Import React for component creation
+import Background from '../Components/LoginSignupPages/Background' // Import Background component
+import Signup from '../Components/LoginSignupPages/Signup' // Import Signup component
 
+// Function component for SignupPage
 function SignupPage() {
   return (
-    <section className='relative w-full h-[42rem] lg:h-[50rem] xl:h-[55rem]'>
-      <Background />
-      <Signup />
+    <section className='relative w-full h-[42rem] lg:h-[50rem] xl:h-[55rem]'> // Container for SignupPage with responsive height
+      <Background /> // Render Background component
+      <Signup /> // Render Signup component
     </section>
   )
 }
 
-export default SignupPage
+export default SignupPage // Export the SignupPage component
