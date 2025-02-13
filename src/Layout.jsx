fcc6cf72fd1c7337e2a4ec31bce4cf6713926081
pravesh dav <humantorch1932@gmail.com>
2025-02-13 20:38:28 +0530

    added comments in layout component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
+import React from 'react' // Import React for component creation
+import { Outlet } from 'react-router-dom' // Import Outlet for nested routes
+import Header from './Components/Header' // Import Header component
+import Footer from './Components/Footer' // Import Footer component
 
+// Function component for the Layout
 function Layout() {
   return (
-    <main className='max-w-[1520px] mx-auto bg-[#FFFFF0]'>
-        <Header/>
-        <Outlet/>
-        <Footer />
+    <main className='max-w-[1520px] mx-auto bg-[#FFFFF0]'> // Main container with styling
+        <Header/> // Header component
+        <Outlet/> // Outlet for nested routes
+        <Footer /> // Footer component
     </main>
   )
 }
 
-export default Layout
+export default Layout // Export the Layout component
