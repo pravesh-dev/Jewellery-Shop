cd2ce79028070b186bc5cea4c9ed61d4988d9f7c
pravesh dav <humantorch1932@gmail.com>
2025-02-13 20:40:24 +0530

    added comments in user cart page

--- src/Pages/UserCartPage.jsx
+++ src/Pages/UserCartPage.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ShoppingCart from '../Components/CartPage/ShoppingCart';
+import React from 'react' // Import React for component creation
+import ShoppingCart from '../Components/CartPage/ShoppingCart'; // Import ShoppingCart component
 
+// Function component for UserCartPage
 function UserCartPage() {
   return (
     <div>
-      <div className='hidden md:block'>
-      <ShoppingCart />
+      <div className='hidden md:block'> // Conditional rendering for different screen sizes
+      <ShoppingCart /> // Render ShoppingCart component
       </div>
     </div>
   )
 }
 
-export default UserCartPage
+export default UserCartPage // Export the UserCartPage component
