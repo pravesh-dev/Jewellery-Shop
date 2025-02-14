daa082088efd499f1e65c008ec3ff971ab7065f8
pravesh dav <humantorch1932@gmail.com>
2025-02-14 19:35:26 +0530

    added basic comments in contact page

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import Contact from '../Components/ContactPage/Contact'
-import BgImage from '../Components/ContactPage/BgImage'
-import FrequentlyQues from '../Components/ContactPage/FrequentlyQues'
+import React from 'react' // Import React for component creation
+import Contact from '../Components/ContactPage/Contact' // Import Contact component from ContactPage
+import BgImage from '../Components/ContactPage/BgImage' // Import BgImage component from ContactPage
+import FrequentlyQues from '../Components/ContactPage/FrequentlyQues' // Import FrequentlyQues component from ContactPage
 
+// Function component for ContactPage
 function ContactPage() {
   return (
-    <>
-      <Contact />
-      <BgImage />
-      <FrequentlyQues />
+    <> // Fragment to wrap components without adding extra DOM nodes
+      <Contact /> // Render Contact component
+      <BgImage /> // Render BgImage component
+      <FrequentlyQues /> // Render FrequentlyQues component
     </>
   )
 }
 
-export default ContactPage
+export default ContactPage // Export the ContactPage component
