6dc9d45e58f90f766daf535d598a004fa32c8fbb
pravesh dav <humantorch1932@gmail.com>
2025-02-14 19:34:41 +0530

    added comments in homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import Hero from '../Components/LandingPage/Hero'
-import FeedBack from '../Components/LandingPage/Feedback'
-import Featured from '../Components/LandingPage/Featured'
-import ShopBy from '../Components/LandingPage/ShopBy'
+import React from 'react' // Import React for component creation
+import Hero from '../Components/LandingPage/Hero' // Import Hero component from LandingPage
+import FeedBack from '../Components/LandingPage/Feedback' // Import Feedback component from LandingPage
+import Featured from '../Components/LandingPage/Featured' // Import Featured component from LandingPage
+import ShopBy from '../Components/LandingPage/ShopBy' // Import ShopBy component from LandingPage
 
+// Function component for Home page
 function Home() {
   return (
-    <>
-    <Hero />
-    <Featured />
-    <ShopBy />
-    <FeedBack />
+    <> // Fragment to wrap components without adding extra DOM nodes
+    <Hero /> // Render Hero component
+    <Featured /> // Render Featured component
+    <ShopBy /> // Render ShopBy component
+    <FeedBack /> // Render Feedback component
     </>
   )
 }
 
-export default Home
-
-
-
-
-
+export default Home // Export the Home component
