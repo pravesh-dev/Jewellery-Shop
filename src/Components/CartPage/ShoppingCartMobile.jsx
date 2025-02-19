73b273f0f5c0f175200f65ff489634ed4922add6
pravesh dav <humantorch1932@gmail.com>
2025-02-19 16:52:54 +0530

    fixed design for mobile cart

--- src/Components/CartPage/ShoppingCartMobile.jsx
+++ src/Components/CartPage/ShoppingCartMobile.jsx
@@ -27,29 +27,29 @@ function ShoppingCartMobile() {
   }, [cartItems, items]);
 
   return (
-    <div className="w-full pt-24 px-2 grid grid-cols-11">
+    <div className="w-full pt-7 px-2">
       <div className="col-span-11">
         <h1 className="text-primary text-stroke-1 mb-1 font-bellefair text-[1.12rem]">
           Shopping Cart
         </h1>
-        <div className="min-h-[83vh] border-t border-r border-[#D9D9D9] pt-5">
+        <div className="min-h-[83vh] border-t pt-5">
           {cartData.length > 0 ? (
             <div className="grid grid-cols-7 mb-5">
-              <div className="col-span-4">
-                <h2 className="text-[1rem] text-dark font-lora">
+              <div className="col-span-3">
+                <h2 className="text-[0.7rem] text-dark font-lora">
                   Product Details
                 </h2>
               </div>
-              <div className="col-span-3">
+              <div className="col-span-4">
                 <div className="grid grid-cols-3 gap-2 font-lora">
-                  <h3 className="text-[1rem] text-dark text-center">Quantity</h3>
-                  <h3 className="text-[1rem] text-dark text-center">Price</h3>
-                  <h3 className="text-[1rem] text-dark text-center">Total</h3>
+                  <h3 className="text-[0.7rem] text-dark text-center">Quantity</h3>
+                  <h3 className="text-[0.7rem] text-dark text-center">Price</h3>
+                  <h3 className="text-[0.7rem] text-dark text-center">Total</h3>
                 </div>
               </div>
             </div>
           ) : (
-            <div className="text-center text-dark font-lora text-[1.12rem]">
+            <div className="text-center text-dark font-lora text-[1rem]">
               Your cart is empty. Please add some products to your cart.
             </div>
           )}
@@ -60,38 +60,38 @@ function ShoppingCartMobile() {
               : product.price * product.quantity;
 
             return (
-              <div key={product.id} className="mb-3 xl:mb-6 grid grid-cols-7">
-                <div className="col-span-4 flex items-start gap-2">
-                  <div className="w-[7.6rem] flex-shrink-0 h-[6.8rem] rounded-[5px] overflow-hidden">
+              <div key={product.id} className="mb-5 grid grid-cols-7">
+                <div className="col-span-3 flex items-start flex-col gap-1">
+                  <div className="w-[4.6rem] flex-shrink-0 h-[3.8rem] rounded-[2px] overflow-hidden">
                     <img src={product.image} className="w-full h-full object-cover" alt="product thumbnail" loading="lazy" />
                   </div>
-                  <div className="font-bellefair pt-5">
-                    <h1 className="text-[1rem] leading-4">{product.name}</h1>
-                    <h3 className="font-Cormorant text-primary text-[0.72rem] italic">In stock</h3>
+                  <div className="font-bellefair pt-2">
+                    <h1 className="text-[0.9rem] leading-4">{product.name}</h1>
+                    <h3 className="font-Cormorant text-primary text-[0.7rem] italic">In stock</h3>
                     {product.onSale && (
                       <p>
-                        <span className="font-lora text-[0.7rem] py-[0.13rem] mr-2 px-2 rounded-[0.23rem] text-accent bg-[#ff0000]">
+                        <span className="font-lora text-[0.6rem] py-[0.1rem] mr-1 px-1 rounded-[0.23rem] text-accent bg-[#ff0000]">
                           {product.discount}% Off
                         </span>
-                        <span className="font-bellefair text-dark text-[0.68rem]">Limited Time Deal</span>
+                        <span className="font-bellefair text-dark text-[0.63rem]">Limited Time Deal</span>
                       </p>
                     )}
-                    <div className="flex gap-2">
+                    <div className="flex gap-4">
                       <button className="text-[#A0A0A0] text-[0.75rem]" onClick={() => removeFromCart(product.id)}>Remove</button>
                       <button className="text-[#A0A0A0] text-[0.75rem]">Share</button>
                     </div>
                   </div>
                 </div>
 
-                <div className="col-span-3 h-[6.8rem] grid content-start pt-5 grid-cols-3 gap-2">
+                <div className="col-span-4 h-[6.8rem] grid content-start pt-5 grid-cols-3 gap-2">
                   <div className="flex justify-center items-start">
-                    <div className="border-2 border-secondary rounded-full flex justify-center items-center text-sm px-2 gap-1">
+                    <div className="border border-secondary rounded-full flex justify-center items-center text-[0.7rem] px-2 gap-1">
                       <HiOutlineMinus className="cursor-pointer" onClick={() => lessFromCart(product.id)} />
                       <span>{product.quantity}</span>
                       <BsPlusLg className="cursor-pointer" onClick={() => addToCart(product.id)} />
                     </div>
                   </div>
-                  <h3 className="text-[1rem] font-bellefair text-center">
+                  <h3 className="text-[0.8rem] font-bellefair text-center">
                     {product.onSale ? (
                       <div className="flex flex-col -space-y-1">
                         <span>{currency} {product.price - product.price * (product.discount / 100)}</span>
@@ -101,7 +101,7 @@ function ShoppingCartMobile() {
                       <span>{currency} {product.price}</span>
                     )}
                   </h3>
-                  <h3 className="font-bellefair text-center text-[1rem]">{currency} {totalPrice}</h3>
+                  <h3 className="font-bellefair text-center text-[0.8rem]">{currency} {totalPrice}</h3>
                 </div>
               </div>
             );
