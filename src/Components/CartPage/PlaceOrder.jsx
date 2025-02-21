38c73b067ccfd9c0c30660ddf6d921404f4d8ca3
pravesh dav <humantorch1932@gmail.com>
2025-02-21 16:41:30 +0530

    added place order right side content

--- src/Components/CartPage/PlaceOrder.jsx
+++ src/Components/CartPage/PlaceOrder.jsx
@@ -1,8 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../../Context/ShopContext';
+import stripe from '../../Assets/stripe.png'
+import razorpay from '../../Assets/razorpay.png'
 
 function PlaceOrder() {
-  
+  const { checkout, currency } = useContext(ShopContext);
+  const [method, setMethod] = useState('cod');
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
@@ -22,7 +25,6 @@ function PlaceOrder() {
     });
   };
 
-  const { checkout, currency } = useContext(ShopContext);
 
   return (
     <div className='w-full min-h-screen pt-24 px-2 lg:pt-32 xl:px-16'>
@@ -52,18 +54,35 @@ function PlaceOrder() {
         <h2 className="text-primary uppercase text-stroke-1 mb-4 font-bellefair text-[1.12rem] lg:text-[2rem]">
           cart totals —
         </h2>
-        <div>
+        <div className='flex justify-between items-center text-[0.8rem] font-mulish py-1 border-b'>
           <h3>Subtotal</h3>
           <p>{checkout.totalPrice ? `${currency} ${checkout.totalPrice}` : 'N/A'}</p>
         </div>
-        <div>
+        <div className='flex justify-between items-center text-[0.8rem] font-mulish py-1 border-b'>
           <h3>Shipping Fee</h3>
           <p>{checkout.shippingCost ? `${currency} ${checkout.shippingCost}` : 'N/A'}</p>
         </div>
-        <div>
+        <div className='flex justify-between items-center text-[0.8rem] font-mulish font-semibold py-1'>
           <h3>Total</h3>
           <p>{checkout.totalCost ? `${currency} ${checkout.totalCost}` : 'N/A'}</p>
         </div>
+        </div>
+        <h2 className="text-primary uppercase text-stroke-1 mb-4 mt-5 font-bellefair text-[0.75rem] tracking-wider lg:text-[2rem]">
+          payment method —
+        </h2>
+        <div className='flex flex-col gap-2 items-start mb-5'>
+          <div className='h-7 px-3 flex gap-4 items-center border' onClick={()=>{setMethod('stripe')}}>
+            <p className={`w-3 h-3 rounded-full border flex-shrink-0 ${method === 'stripe' ? 'bg-green-500' : ''}`}></p>
+            <img className='w-12' src={stripe} alt="stripe logo" />
+          </div>
+          <div className='h-7 px-3 flex gap-4 items-center border' onClick={()=>{setMethod('razorpay')}}>
+            <p className={`w-3 h-3 rounded-full border flex-shrink-0 ${method === 'razorpay' ? 'bg-green-500' : ''}`}></p>
+            <img className='w-12' src={razorpay} alt="razorpay logo" />
+          </div>
+          <div className='h-7 px-3 flex gap-4 items-center border' onClick={()=>{setMethod('cod')}}>
+            <p className={`w-3 h-3 rounded-full border flex-shrink-0 ${method === 'cod' ? 'bg-green-500' : ''}`}></p>
+            <p className='uppercase text-[0.6rem] font-mulish font-semibold'>cash on delivery</p>
+          </div>
         </div>
     </div>
   )
