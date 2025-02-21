e58b6d8f78c15348e3376324ee8efe99adfb6f6b
pravesh dav <humantorch1932@gmail.com>
2025-02-21 18:15:22 +0530

    writing order page

--- src/Pages/OrderPage.jsx
+++ src/Pages/OrderPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
 
 function OrderPage() {
+    const { items, currency } = useContext(ShopContext);
   return (
-    <div className='w-full min-h-screen'>
-      
+    <div className='w-full min-h-screen pt-24 px-2 lg:pt-32 lg:px-10 xl:px-32'>
+      <h2 className="text-primary uppercase text-stroke-1 mb-4 font-bellefair text-[1.12rem]">
+          My orders
+        </h2>
+        <div>
+            {
+                items.slice(1,4).map((item, index)=>{
+                    <div key={index} className=''>
+
+                    </div>
+                })
+            }
+        </div>
     </div>
   )
 }
