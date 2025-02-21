0dc4990148d790894584366ad1de9e0d2caa067c
pravesh dav <humantorch1932@gmail.com>
2025-02-21 15:35:12 +0530

    design form

--- src/Components/CartPage/PlaceOrder.jsx
+++ src/Components/CartPage/PlaceOrder.jsx
@@ -23,25 +23,25 @@ function PlaceOrder() {
 
   return (
     <div className='w-full min-h-screen pt-24 px-2 lg:pt-32 xl:px-16'>
-      <h1 className="text-primary uppercase text-stroke-1 mb-1 font-bellefair text-[1.12rem] lg:text-[2rem]">
+      <h1 className="text-primary uppercase text-stroke-1 mb-4 font-bellefair text-[1.12rem] lg:text-[2rem]">
           Delivery information
         </h1>
-        <form>
-            <div className='flex w-full'>
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.firstName} onChange={handleInputChange} name="firstName" type="text" placeholder='First name' required autoComplete='off' />
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.lastName} onChange={handleInputChange} name="lastName" type="text" placeholder='Last name' required autoComplete='off' />
+        <form className='space-y-2'>
+            <div className='flex w-full space-x-2'>
+                <input className='w-1/2 h-10 border border-y-2 border-black/20 outline-none text-[0.8rem] pl-2' value={formData.firstName} onChange={handleInputChange} name="firstName" type="text" placeholder='First name' required autoComplete='off' />
+                <input className='w-1/2 h-10 border border-y-2 border-black/20 outline-none text-[0.8rem] pl-2' value={formData.lastName} onChange={handleInputChange} name="lastName" type="text" placeholder='Last name' required autoComplete='off' />
             </div>
-            <input className='w-full h-10 border outline-none text-[0.8rem] pl-2' value={formData.email} onChange={handleInputChange} name="email" type="email" placeholder='Email address' required autoComplete='off' />
-            <input className='w-full h-10 border outline-none text-[0.8rem] pl-2' value={formData.street} onChange={handleInputChange} name="street" type="text" placeholder='Street' required autoComplete='off' />
-            <div className='flex w-full'>
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.city} onChange={handleInputChange} name="city" type="text" placeholder='City' required autoComplete='off' />
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.state} onChange={handleInputChange} name="state" type="text" placeholder='State' required autoComplete='off' />
+            <input className='w-full h-10 border border-y-2 border-black/20 outline-none text-[0.8rem] pl-2' value={formData.email} onChange={handleInputChange} name="email" type="email" placeholder='Email address' required autoComplete='off' />
+            <input className='w-full h-10 border border-y-2 border-black/20 outline-none text-[0.8rem] pl-2' value={formData.street} onChange={handleInputChange} name="street" type="text" placeholder='Street' required autoComplete='off' />
+            <div className='flex w-full space-x-2'>
+                <input className='w-1/2 h-10 border border-y-2 border-black/20 outline-none text-[0.8rem] pl-2' value={formData.city} onChange={handleInputChange} name="city" type="text" placeholder='City' required autoComplete='off' />
+                <input className='w-1/2 h-10 border border-y-2 border-black/20 outline-none text-[0.8rem] pl-2' value={formData.state} onChange={handleInputChange} name="state" type="text" placeholder='State' required autoComplete='off' />
             </div>
-            <div className='flex w-full'>
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.zipcode} onChange={handleInputChange} name="zipcode" type="text" placeholder='Zipcode' required autoComplete='off' />
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.country} onChange={handleInputChange} name="country" type="text" placeholder='Country' required autoComplete='off' />
+            <div className='flex w-full space-x-2'>
+                <input className='w-1/2 h-10 border border-y-2 border-black/20 outline-none text-[0.8rem] pl-2' value={formData.zipcode} onChange={handleInputChange} name="zipcode" type="text" placeholder='Zipcode' required autoComplete='off' />
+                <input className='w-1/2 h-10 border border-y-2 border-black/20 outline-none text-[0.8rem] pl-2' value={formData.country} onChange={handleInputChange} name="country" type="text" placeholder='Country' required autoComplete='off' />
             </div>
-            <input className='w-full h-10 border outline-none text-[0.8rem] pl-2' value={formData.phone} onChange={handleInputChange} name="phone" type="number" placeholder='Phone' required autoComplete='off' />
+            <input className='w-full h-10 border border-y-2 border-black/20 outline-none text-[0.8rem] pl-2' value={formData.phone} onChange={handleInputChange} name="phone" type="number" placeholder='Phone' required autoComplete='off' />
         </form>
     </div>
   )
