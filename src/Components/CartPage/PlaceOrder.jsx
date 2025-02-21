786c7c0ee7ded1b6eb83dae28caf868e30076a9e
pravesh dav <humantorch1932@gmail.com>
2025-02-21 15:28:10 +0530

    two-way binding for inputs

--- src/Components/CartPage/PlaceOrder.jsx
+++ src/Components/CartPage/PlaceOrder.jsx
@@ -3,9 +3,23 @@ import React, { useState } from 'react'
 function PlaceOrder() {
 
   const [formData, setFormData] = useState({
-    name: '',
-    
+    firstName: '',
+    lastName: '',
+    email: '',
+    street: '',
+    city: '',
+    state: '',
+    zipcode: '',
+    country: '',
+    phone: '',
   })
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div className='w-full min-h-screen pt-24 px-2 lg:pt-32 xl:px-16'>
@@ -14,20 +28,20 @@ function PlaceOrder() {
         </h1>
         <form>
             <div className='flex w-full'>
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.firstName} onChange={(e)=>{setFormData(...formData, e.target.name = e.target.value)}} type="text" placeholder='First name' required autoComplete='off' />
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.lastName} onChange={(e)=>{setFormData(...formData, e.target.name = e.target.value)}} type="text" placeholder='Last name' required autoComplete='off' />
+                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.firstName} onChange={handleInputChange} name="firstName" type="text" placeholder='First name' required autoComplete='off' />
+                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.lastName} onChange={handleInputChange} name="lastName" type="text" placeholder='Last name' required autoComplete='off' />
             </div>
-            <input className='w-full h-10 border outline-none text-[0.8rem] pl-2' value={formData.email} onChange={(e)=>{setFormData(...formData, e.target.name = e.target.value)}} type="email" placeholder='Email address' required autoComplete='off' />
-            <input className='w-full h-10 border outline-none text-[0.8rem] pl-2' value={formData.street} onChange={(e)=>{setFormData(...formData, e.target.name = e.target.value)}} type="text" placeholder='Street' required autoComplete='off' />
+            <input className='w-full h-10 border outline-none text-[0.8rem] pl-2' value={formData.email} onChange={handleInputChange} name="email" type="email" placeholder='Email address' required autoComplete='off' />
+            <input className='w-full h-10 border outline-none text-[0.8rem] pl-2' value={formData.street} onChange={handleInputChange} name="street" type="text" placeholder='Street' required autoComplete='off' />
             <div className='flex w-full'>
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.city} onChange={(e)=>{setFormData(...formData, e.target.name = e.target.value)}} type="text" placeholder='City' required autoComplete='off' />
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.state} onChange={(e)=>{setFormData(...formData, e.target.name = e.target.value)}} type="text" placeholder='State' required autoComplete='off' />
+                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.city} onChange={handleInputChange} name="city" type="text" placeholder='City' required autoComplete='off' />
+                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.state} onChange={handleInputChange} name="state" type="text" placeholder='State' required autoComplete='off' />
             </div>
             <div className='flex w-full'>
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.zipcode} onChange={(e)=>{setFormData(...formData, e.target.name = e.target.value)}} type="text" placeholder='Zipcode' required autoComplete='off' />
-                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.country} onChange={(e)=>{setFormData(...formData, e.target.name = e.target.value)}} type="text" placeholder='Country' required autoComplete='off' />
+                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.zipcode} onChange={handleInputChange} name="zipcode" type="text" placeholder='Zipcode' required autoComplete='off' />
+                <input className='w-1/2 h-10 border outline-none text-[0.8rem] pl-2' value={formData.country} onChange={handleInputChange} name="country" type="text" placeholder='Country' required autoComplete='off' />
             </div>
-            <input className='w-full h-10 border outline-none text-[0.8rem] pl-2' value={formData.name} onChange={(e)=>{setFormData(...formData, e.target.name = e.target.value)}} type="number" placeholder='Phone' required autoComplete='off' />
+            <input className='w-full h-10 border outline-none text-[0.8rem] pl-2' value={formData.phone} onChange={handleInputChange} name="phone" type="number" placeholder='Phone' required autoComplete='off' />
         </form>
     </div>
   )
