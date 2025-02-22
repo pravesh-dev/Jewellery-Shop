397502546f2ae55ae5756092103726f8bdef750e
pravesh dav <humantorch1932@gmail.com>
2025-02-22 20:36:51 +0530

    fix comments for wishlist page

--- src/Pages/WishlistPage.jsx
+++ src/Pages/WishlistPage.jsx
@@ -5,13 +5,13 @@ import LargeWishlist from '../Components/WishlistPage/LargeWishlist'; // Import 
 // Function component for WishlistPage
 function WishlistPage() {
   return (
-    <div className='w-full pt-24 pb-4 px-2 flex flex-col lg:pt-32 xl:px-16'> // Container for the WishlistPage
-      <h1 className='text-primary text-[1.5rem] text-stroke-1 font-bellefair xl:text-[2rem]'>Wishlist</h1> // Wishlist title
-      <span className='w-full h-[1px] bg-[#D9D9D9] my-1 lg:my-2'></span> // Horizontal rule for separation
-      <div className='sm:hidden'> // Mobile view
+    <div className='w-full pt-24 pb-4 px-2 flex flex-col lg:pt-32 xl:px-16'>
+      <h1 className='text-primary text-[1.5rem] text-stroke-1 font-bellefair xl:text-[2rem]'>Wishlist</h1>
+      <span className='w-full h-[1px] bg-[#D9D9D9] my-1 lg:my-2'></span>
+      <div className='sm:hidden'>
         <MobileWishlist />
       </div>
-      <div className='hidden sm:block'> // Large screen view
+      <div className='hidden sm:block'>
         <LargeWishlist />
       </div>
     </div>
