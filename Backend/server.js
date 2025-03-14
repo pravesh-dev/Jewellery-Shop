de42d9f4909fd138a211051609174fae4452ff3e
pravesh dav <humantorch1932@gmail.com>
2025-03-14 20:59:18 +0530

    written api endpoints

--- Backend/server.js
+++ Backend/server.js
@@ -11,3 +11,11 @@ const port = process.env.PORT || 4000;
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+// api endpoints
+
+app.get('/', (req, res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=> console.log('Server started on PORT : ' + port))
