bfded9450d897a5d78cb5f408537830d7572c3b6
pravesh dav <humantorch1932@gmail.com>
2025-03-15 20:12:11 +0530

    connected daa base in server js file

--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,13 @@ import express from 'express';
 import cors from 'cors';
 
 import 'dotenv/config';
+import connectDB from './config/mongodb.js';
 
 // App config
 const app = express();
 const port = process.env.PORT || 4000;
+
+connectDB();
 
 // middlewares
 app.use(express.json());
