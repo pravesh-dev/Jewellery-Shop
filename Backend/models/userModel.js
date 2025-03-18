1a3afbe179f78b708c8be3c164187e27a9730c32
pravesh dav <humantorch1932@gmail.com>
2025-03-18 20:31:07 +0530

    added coments in usermodel

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,12 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"; // Importing mongoose for MongoDB operations
 
+// Defining the schema for the user model
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    cartData: {type: Object, default: {}},
-}, {minimize: false})
+    name: {type: String, required: true}, // User's name
+    email: {type: String, required: true, unique: true}, // User's email, required and unique
+    password: {type: String, required: true}, // User's password, required
+    cartData: {type: Object, default: {}}, // User's cart data, default is an empty object
+}, {minimize: false}) // Schema options to prevent MongoDB from removing empty objects
 
+// Creating or retrieving the user model
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
 
+// Exporting the user model
 export default userModel;
