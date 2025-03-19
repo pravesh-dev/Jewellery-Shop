4f10613cb218187bd17298dd505e9fb32b0b1332
pravesh dav <humantorch1932@gmail.com>
2025-03-19 22:42:52 +0530

    added comments in cloudinary

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -1,13 +1,15 @@
+// Importing the cloudinary library
 import { v2 as cloudinary } from 'cloudinary';
 
+// Function to connect to Cloudinary
 const connnectCloudinary = async ()=>{
-
+    // Configuring Cloudinary with environment variables
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_SECRET_KEY
     })
-
 }
 
+// Exporting the function to connect to Cloudinary
 export default connnectCloudinary;
