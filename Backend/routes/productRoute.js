23cb4348b70d291caeeb3fa66b5aff01568e5ec5
pravesh dav <humantorch1932@gmail.com>
2025-03-26 21:12:16 +0530

    added routes for productroute

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,2 +1,11 @@
 import express from "express";
 import { addProduct, listProducts, singleProduct, removeProduct } from "../controllers/productController.js";
+
+const productRouter = express.Router();
+
+productRouter.post('/add', addProduct);
+productRouter.post('/remove', removeProduct);
+productRouter.post('/single', singleProduct);
+productRouter.post('/list', listProducts);
+
+export default productRouter;
