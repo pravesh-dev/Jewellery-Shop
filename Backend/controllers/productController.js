4e300f9de8e9dcbb81931df7384b88870a8cfb4f
pravesh dav <humantorch1932@gmail.com>
2025-03-28 20:18:37 +0530

    containing image in array

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -9,8 +9,11 @@ const addProduct = async (req, res) => {
     const image3 = req.files.image3 && req.files.image3[0];
     const image4 = req.files.image4 && req.files.image4[0];
 
-    console.log(name, title, description, price, onSale, discount, image, category, subCategory, bestSeller);
-    console.log(image1, image2, image3, image4)
+
+    const images = [image1, image2, image3, image4].filter((item)=> {item !== undefined})
+
+    console.log(name, title, description, price, onSale, discount, category, subCategory, bestSeller);
+    console.log(images)
 
     res.json({})
     } catch (error) {
