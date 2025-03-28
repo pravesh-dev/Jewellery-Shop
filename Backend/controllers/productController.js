3ad6e72e70d3412e33898d70e6348871a3ad3719
pravesh dav <humantorch1932@gmail.com>
2025-03-28 20:16:24 +0530

    erroe handling of images not provided

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -4,10 +4,10 @@ const addProduct = async (req, res) => {
     try {
         const {name, title, description, price, onSale, discount, image, category, subCategory, bestSeller} = req.body;
 
-    const image1 = req.files.image1[0];
-    const image2 = req.files.image2[0];
-    const image3 = req.files.image3[0];
-    const image4 = req.files.image4[0];
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
 
     console.log(name, title, description, price, onSale, discount, image, category, subCategory, bestSeller);
     console.log(image1, image2, image3, image4)
