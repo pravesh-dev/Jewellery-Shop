bd6bf12408aa1be6a04f9c22ac2123617ae15448
pravesh dav <humantorch1932@gmail.com>
2025-04-01 21:29:41 +0530

    added coments in productmodel

--- Backend/models/productModel.js
+++ Backend/models/productModel.js
@@ -1,20 +1,23 @@
 import mongoose from "mongoose";
 
+// Defining the schema for the product model
 const productSchema = new mongoose.Schema({
-    id: {type: Number, required: true},
-    name: {type: String, required: true},
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    onSale: {type: Boolean, required: true},
-    discount: {type: Number, required: true},
-    image: {type: Array, required: true},
-    category: {type: String, required: true},
-    subCategory: {type: String, required: true},
-    bestSeller: {type: Boolean},
-    date: {type: Number, required: true},
+    id: {type: Number, required: true}, // Product ID
+    name: {type: String, required: true}, // Product name
+    title: {type: String, required: true}, // Product title
+    description: {type: String, required: true}, // Product description
+    price: {type: Number, required: true}, // Product price
+    onSale: {type: Boolean, required: true}, // Product on sale status
+    discount: {type: Number, required: true}, // Product discount
+    image: {type: Array, required: true}, // Product images
+    category: {type: String, required: true}, // Product category
+    subCategory: {type: String, required: true}, // Product sub-category
+    bestSeller: {type: Boolean}, // Product best seller status
+    date: {type: Number, required: true}, // Product creation date
 })
 
+// Creating or retrieving the product model
 const productModel = mongoose.models.product || mongoose.model("product", productSchema);
 
+// Exporting the product model
 export default productModel;
