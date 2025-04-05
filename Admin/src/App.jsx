98adb2ac19d5785281295c23d5fabd2abe0ae6d7
pravesh dav <humantorch1932@gmail.com>
2025-04-05 22:47:22 +0530

    added coments in app js

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react' // Importing React from the react library
 
+// Defining the App component
 function App() {
+  // Returning the JSX for the App component
   return (
-    <div className='bg-red-500'>
-      home admin page
+    <div className='bg-red-500'> // Div with a red background
+      home admin page // Text content of the div
     </div>
   )
 }
 
+// Exporting the App component as the default export
 export default App
