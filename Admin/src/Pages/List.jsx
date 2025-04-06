5bcc7c207760b58dda9058fd888cd0001fd01f3e
pravesh dav <humantorch1932@gmail.com>
2025-04-06 22:58:06 +0530

    removed comments from list

--- Admin/src/Pages/List.jsx
+++ Admin/src/Pages/List.jsx
@@ -1,14 +1,11 @@
-import React from 'react' // Importing React from the react library
+import React from 'react'
 
-// Defining the List component
 const List = () => {
-  // Returning the JSX for the List component
   return (
     <div>
-      List page // Text content of the div
+      List page
     </div>
   )
 }
 
-// Exporting the List component as the default export
 export default List
