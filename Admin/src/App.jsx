bb8329d703354aeb6bb406914ae382da920029de
pravesh dav <humantorch1932@gmail.com>
2025-04-07 23:23:14 +0530

    a

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,32 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Dashboard from './Dashboard';
-import Products from './Products';
-import Users from './Users';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/products">Products</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/products" component={Products} />
-        <Route path="/users" component={Users} />
-      </div>
-    </Router>
+    <div>
+      <h1>Home Admin Page</h1>
+    </div>
   );
 }
 
