6f2106a877f9110b23757d23af4112079519958c
pravesh dav <humantorch1932@gmail.com>
2025-04-07 19:20:37 +0530

    s

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,14 +1,11 @@
-import React from 'react' // Importing React from the react library
+import React from 'react';
 
-// Defining the App component
 function App() {
-  // Returning the JSX for the App component
   return (
-    <div className='bg-red-500'> // Div with a red background
-      home admin page // Text content of the div
+    <div>
+      <h1>Home Admin Page</h1>
     </div>
-  )
+  );
 }
 
-// Exporting the App component as the default export
-export default App
+export default App;
