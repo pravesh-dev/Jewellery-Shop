3010ba961fc708289f1b9aad919206a5833536b5
pravesh dav <humantorch1932@gmail.com>
2025-04-09 20:22:15 +0530

    styled navbar for admin

--- Admin/src/Components/Navbar.jsx
+++ Admin/src/Components/Navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 import logo from "../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <div className=''>
-      <img src={logo} alt="logo image" />
-      <button>Logout</button>
+    <div className="w-full flex justify-between items-center py-2 px-4">
+      <img className="w-16 lg:w-20" src={logo} alt="logo image" />
+      <button
+        className={`px-5 py-2 rounded-full bg-gray-600 duration-300 text-white text-center tracking-[1px] text-xs sm:text-sm sm:px-7`}
+      >
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
