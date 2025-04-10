f59e0a1481624af94c359fa2f4ba72e181b92479
pravesh dav <humantorch1932@gmail.com>
2025-04-10 14:17:43 +0530

    added hover effect in sidebar

--- Admin/src/Components/Sidebar.jsx
+++ Admin/src/Components/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
     <div className="w-[18%] min-h-screen border-r-1">
       <div className="flex flex-col gap-4 pt-6 pl-[20%] text-sm text-gray-900 mulish">
         <NavLink
-          className="flex items-center gap-3 border border-r-0 border-gray-300 px-3 py-2 "
+          className="flex items-center gap-3 duration-300 border border-r-0 border-gray-300 px-3 py-2 "
           to="/add"
         >
           <IoAddCircleOutline className="text-xl" />
@@ -16,7 +16,7 @@ const Sidebar = () => {
         </NavLink>
 
         <NavLink
-          className="flex items-center gap-3 border border-r-0 border-gray-300 px-3 py-2 "
+          className="flex items-center gap-3 duration-300 border border-r-0 border-gray-300 px-3 py-2 "
           to="/list"
         >
           <BsCalendarCheck className="text-xl" />
@@ -24,7 +24,7 @@ const Sidebar = () => {
         </NavLink>
 
         <NavLink
-          className="flex items-center gap-3 border border-r-0 border-gray-300 px-3 py-2 "
+          className="flex items-center gap-3 duration-300 border border-r-0 border-gray-300 px-3 py-2 "
           to="/order"
         >
           <BsCalendarCheck className="text-xl" />
