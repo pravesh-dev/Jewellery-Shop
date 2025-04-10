6b43e0e15d68e0e82cd37f4a6faed9faefc138be
pravesh dav <humantorch1932@gmail.com>
2025-04-10 13:40:46 +0530

    added fonts in admin

--- Admin/src/Components/Navbar.jsx
+++ Admin/src/Components/Navbar.jsx
@@ -3,8 +3,11 @@ import logo from "../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between items-center py-1 px-4">
-      <img className="w-16 lg:w-20" src={logo} alt="logo image" />
+    <div className="w-full flex justify-between items-center py-3 px-4 lg:py-5">
+      <p className="flex items-center">
+      {/* <img className="w-14 lg:w-20" src={logo} alt="logo image" /> */}
+      <h1 className="mulish uppercase text-xl lg:text-2xl">joyería</h1>
+      </p>
       <button
         className={`px-5 py-2 rounded-full bg-gray-600 duration-300 text-white text-center tracking-[1px] text-xs sm:text-sm sm:px-7`}
       >
