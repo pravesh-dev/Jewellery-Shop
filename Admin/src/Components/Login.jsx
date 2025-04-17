ff49b974860b4331653fe6d5518ea337a9f134b0
pravesh dav <humantorch1932@gmail.com>
2025-04-17 17:01:08 +0530

    styled login page for admin

--- Admin/src/Components/Login.jsx
+++ Admin/src/Components/Login.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className='min-h-screen flex items-center justify-center w-full'>
+      <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md'>
+        <h1 className='text-2xl mb-4 mulish'>Admin Panel</h1>
+        <form className='bellefair'>
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm text-gray-700 mb-2'>Email Address</p>
+                <input className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='your@email.com' required />
+            </div>
+            <div className='mb-3 min-w-72'>
+                <p className='text-sm text-gray-700 mb-2'>Password</p>
+                <input className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="password" placeholder='Your password' required />
+            </div>
+            <button type='submit' className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black cursor-pointer lora tracking-wider'>Login</button>
+        </form>
+      </div>
     </div>
   )
 }
