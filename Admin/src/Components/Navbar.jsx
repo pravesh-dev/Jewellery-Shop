50625b91c370bfefb40e8c1dba12230899858308
pravesh dav <humantorch1932@gmail.com>
2025-04-21 18:58:02 +0530

    logout fun in nav bar

--- Admin/src/Components/Navbar.jsx
+++ Admin/src/Components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = ({setToken}) => {
       <h1 className="mulish uppercase text-xl lg:text-2xl">joyería</h1>
       {/* </p> */}
       <button
+      onClick={()=>setToken('')}
         className={`px-5 py-2 rounded-full bg-gray-600 hover:bg-gray-800 cursor-pointer duration-300 text-white text-center tracking-[1px] text-xs sm:text-sm sm:px-7`}
       >
         Logout
