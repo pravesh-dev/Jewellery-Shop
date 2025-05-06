f0e91feb5794399a48cb0c5e2e907d6572b4bb8a
pravesh dav <humantorch1932@gmail.com>
2025-05-06 17:44:19 +0530

    created stated for add product

--- Admin/src/Pages/Add.jsx
+++ Admin/src/Pages/Add.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Defining the Add component
 const Add = () => {
+
+  const [image1, setImage1] = useState(false);
+  const [image2, setImage2] = useState(false);
+  const [image3, setImage3] = useState(false);
+  const [image4, setImage4] = useState(false);
+  
+  const [name, setName] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [onSale, setOnSale] = useState(false);
+  const [discount, setDiscount] = useState('');
+  const [category, setCategory] = useState('Men');
+  const [subCategory, setSubCategory] = useState('Ring');
+  const [bestseller, setBestseller] = useState(false);
+
   // Returning the JSX for the Add component
   return (
     <div>
-      Add product home page
+      
     </div>
   )
 }
