de63c28790ec9179c1e4305e889925597174f82e
pravesh dav <humantorch1932@gmail.com>
2025-05-23 17:14:49 +0530

    product image change onclick in product page

--- Frontend/src/Components/ProductPage/ImagesSection.jsx
+++ Frontend/src/Components/ProductPage/ImagesSection.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
+
 function ImagesSection({product}) {
+  const [activeImage, setActiveImage] = useState(product.image[0])
   return (
     <div className="w-72 flex flex-col gap-3 md:w-80 lg:w-96 xl:w-[26.4rem] xl:gap-4">
       <div className="w-full h-80 overflow-hidden rounded-xl lg:h-96 xl:h-[24.7rem]">
         <img
-          src={product.image[0]}
+          src={activeImage}
           className="w-full h-full object-cover"
           alt="product image"
         />
       </div>
       <div className="w-full flex gap-2 xl:justify-between">
         {product.image.slice(0, 3).map((img, index) => (
-          <div key={index} className="w-1/4 h-20 overflow-hidden rounded-lg lg:h-20 xl:h-[5.8rem] xl:rounded-xl xl:w-[5.9rem]">
+          <div key={index} className="w-1/4 h-20 overflow-hidden rounded-lg lg:h-20 xl:h-[5.8rem] xl:rounded-xl xl:w-[5.9rem]" onClick={()=> setActiveImage(img)}>
             <img
               src={img}
               className="w-full h-full object-cover"
@@ -19,7 +22,7 @@ function ImagesSection({product}) {
           </div>
         ))}
         {product.image.length === 4 && (
-          <div className="w-1/4 h-20 overflow-hidden rounded-lg lg:h-20 xl:h-[5.8rem] xl:rounded-xl xl:w-[5.9rem]">
+          <div className="w-1/4 h-20 overflow-hidden rounded-lg lg:h-20 xl:h-[5.8rem] xl:rounded-xl xl:w-[5.9rem]" onClick={()=> setActiveImage(product.image[3])}>
             <img
               src={product.image[3]}
               className="w-full h-full object-cover"
