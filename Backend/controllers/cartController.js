0beab65ceeb4548e920581a85bf01185747b7844
pravesh dav <humantorch1932@gmail.com>
2025-05-27 12:33:10 +0530

    created cart controller and route files

---
+++ Backend/controllers/cartController.js
@@ -0,0 +1,25 @@
+
+
+
+// Add products to user cart 
+const addToCart = async (req, res) => {
+    
+}
+
+
+
+
+// Update user cart
+const updateCart = async (req, res) => {
+
+}
+
+
+
+// Get user cart data
+const getUserCart = async (req, res) => {
+
+}
+
+
+export { addToCart, updateCart, getUserCart }
---
+++ Backend/routes/cartRoute.js
@@ -0,0 +1,2 @@
+import express from 'express';
+import { addToCart, getUserCart, updateCart } from '../controllers/cartController';
