41a74acb9de2ea147c4e997172fe7b0bf32979eb
pravesh dav <humantorch1932@gmail.com>
2025-05-27 13:02:43 +0530

    written addtocart, getcart and updatecart controller function

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -1,9 +1,31 @@
-
+import userModel from "../models/userModel.js";
 
 
 // Add products to user cart 
 const addToCart = async (req, res) => {
     
+    try {
+        
+        const { userId, itemId } = req.body;
+
+        const userData = await userModel.findById(userId);
+        const cartData = await userData.cartData;
+
+        if(cartData[itemId]) {
+            cartData[itemId] += 1
+        } else {
+            cartData[itemId] = {}
+        }
+
+        await userModel.findByIdAndUpdate(userId, {cartData});
+        
+        res.json({success:true, message: "Added To Cart"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+
 }
 
 
@@ -12,12 +34,44 @@ const addToCart = async (req, res) => {
 // Update user cart
 const updateCart = async (req, res) => {
 
+    try {
+
+        const { userId, itemId, quantity } = req.body;
+        
+        const userData = await userModel.findById(userId);
+        const cartData = await userData.cartData;
+
+        cartData[itemId] = quantity
+
+        await userModel.findByIdAndUpdate(userId, {cartData});
+        
+        res.json({success:true, message: "Cart Updated"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+
 }
 
 
 
 // Get user cart data
 const getUserCart = async (req, res) => {
+
+    try {
+        
+        const { userId } = req.body;
+
+        const userData = await userModel.findById(userId);
+        const cartData = await userData.cartData;
+
+        res.json({success:true, cartData})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 
 }
 
