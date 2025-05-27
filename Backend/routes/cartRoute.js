921335d91cf8dfaa2f6d78a5ff6f4699fe6f3254
pravesh dav <humantorch1932@gmail.com>
2025-05-27 12:39:09 +0530

    created cart router

--- Backend/routes/cartRoute.js
+++ Backend/routes/cartRoute.js
@@ -1,2 +1,11 @@
 import express from 'express';
-import { addToCart, getUserCart, updateCart } from '../controllers/cartController';
+import { addToCart, getUserCart, updateCart } from '../controllers/cartController.js';
+
+const cartRouter = express.Router();
+
+
+cartRouter.post('/get', getUserCart);
+cartRouter.post('/add', addToCart);
+cartRouter.post('/update', updateCart);
+
+export default cartRouter
--- Backend/server.js
+++ Backend/server.js
@@ -1,39 +1,34 @@
-// Loading environment variables
 import 'dotenv/config';
-
-// Importing necessary modules
 import express from 'express';
 import cors from 'cors';
 
-// Importing database connection function
 import connectDB from './config/mongodb.js';
-// Importing Cloudinary connection function
+
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
-// Creating the Express app and setting the port
+
 const app = express();
 const port = process.env.PORT || 4000;
 
-// Connecting to the database
-connectDB();
 
-// Connecting to Cloudinary
+connectDB();
 connectCloudinary();
 
-// Middleware to parse JSON bodies
 app.use(express.json());
-// Middleware to enable CORS
 app.use(cors());
+
+
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter); 
 
 // API endpoint to test if the API is working
 app.get('/', (req, res)=>{
     res.send("API Working")
 })
-
-app.use('/api/user', userRouter);
-app.use('/api/product', productRouter)
 
 // Starting the server and logging the port
 app.listen(port, ()=> console.log('Server started on PORT : ' + port))
