fa392d513fe4bef233cf40525a4a22d88bfc27e9
pravesh dav <humantorch1932@gmail.com>
2025-06-09 19:20:28 +0530

    written api to load all users orders for admin

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -49,6 +49,16 @@ const placeOrderRazorpay = async (req, res) => {
 // All orders data for admin panel
 const allOrders = async (req, res) => {
     
+    try {
+
+        const orders = await orderModel.find({});
+        res.json({success: true, orders});
+        
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+
 }
 
 
