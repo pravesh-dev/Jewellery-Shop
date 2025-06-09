7f43bd792a2b6d9cbe66a74555face1ebaa8a2c1
pravesh dav <humantorch1932@gmail.com>
2025-06-09 20:27:05 +0530

    a

--- Admin/src/Pages/Orders.jsx
+++ Admin/src/Pages/Orders.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useState } from "react"
 
-const Orders = () => {
+const Orders = ({token}) => {
+
+  const [orders, setOrders] = useState([]);
+
   return (
     <div>
       orders page
