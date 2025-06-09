0daa39a07c8ad081aa85c433861dc4d924f9a5f5
pravesh dav <humantorch1932@gmail.com>
2025-06-09 11:00:47 +0530

    written place order function for cod

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,8 +1,36 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
 
 
 // Place order using cash on delivery method
 const placeOrder = async (req, res) => {
     
+    try {
+        
+        const { userId, items, amount, address } = req.body;
+
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod : 'COD',
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData);
+        await newOrder.save()
+
+        await userModel.findByIdAndUpdate(userId, {cartData: {}});
+
+        res.json({success: true, message: 'Order Placed'});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+
 }
 
 
