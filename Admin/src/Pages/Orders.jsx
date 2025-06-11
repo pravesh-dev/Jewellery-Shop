6cd25d247c705f933554ed41186730977fdae58b
pravesh dav <humantorch1932@gmail.com>
2025-06-11 18:12:13 +0530

    getting orders list in admin panel

--- Admin/src/Pages/Orders.jsx
+++ Admin/src/Pages/Orders.jsx
@@ -1,14 +1,39 @@
-import { useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { backendUrl } from "../App";
+import { toast } from "react-toastify";
 
-const Orders = ({token}) => {
-
+const Orders = ({ token }) => {
   const [orders, setOrders] = useState([]);
 
-  return (
-    <div>
-      orders page
-    </div>
-  )
-}
+  const fetchAllOrders = async () => {
 
-export default Orders
+    if(!token){
+      return null
+    }
+
+    try {
+      
+      const response = await axios.post(backendUrl + 'api/order/list', {}, {headers: {token}});
+
+      if(response.data.success) {
+        setOrders(response.data.orders);
+      } else{
+        toast.error(response.data.message)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message);
+    }
+
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [token]);
+
+  return <div>orders page</div>;
+};
+
+export default Orders;
