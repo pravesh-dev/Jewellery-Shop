95ae237696932085036e4f6e8b40c496008668bc
pravesh dav <humantorch1932@gmail.com>
2025-06-12 18:29:22 +0530

    little styling in orders list in admin panel

--- Admin/src/Pages/Orders.jsx
+++ Admin/src/Pages/Orders.jsx
@@ -39,27 +39,27 @@ const Orders = ({ token }) => {
       <h3>Order Page</h3>
       <div>
         {orders.map((order, index) => (
-          <div key={index}>
-            <img className="w-10" src={parcelIcon} alt="parcel icon" />
+          <div key={index} className="grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 my-3 md:my-4 text-xs sm:text-sm text-gray-700">
+            <img className="w-12 border-2 border-gray-200 p-2 sm:w-16" src={parcelIcon} alt="parcel icon" />
             <div>
               <div>
                 {order.items.map((item, index) => {
                   if (index === order.items.length - 1) {
                     return (
-                      <p key={index}>
+                      <p key={index} className="py-0.5">
                         {item.name} x {item.quantity}
                       </p>
                     );
                   } else {
                     return (
-                      <p key={index}>
+                      <p key={index} className="py-0.5">
                         {item.name} x {item.quantity} ,
                       </p>
                     );
                   }
                 })}
               </div>
-              <p>{order.address.firstName + " " + order.address.lastName}</p>
+              <p className="mt-3 mb-2 font-medium">{order.address.firstName + " " + order.address.lastName}</p>
               <div>
                 <p>{order.address.street + ","}</p>
                 <p>
@@ -75,13 +75,13 @@ const Orders = ({ token }) => {
               <p>{order.address.phone}</p>
             </div>
             <div>
-              <p>Items : {order.items.length}</p>
-              <p>Method : {order.paymentMethod}</p>
+              <p className="text-sm sm:text-base">Items : {order.items.length}</p>
+              <p className="mt-3">Method : {order.paymentMethod}</p>
               <p>Payment : {order.payment ? 'Done' : 'Pending'}</p>
               <p>Date : {new Date(order.date).toLocaleDateString()}</p>
             </div>
-            <p>{currency}{order.amount}</p>
-            <select>
+            <p className="text-sm sm:text-base">{currency}{order.amount}</p>
+            <select value={order.status} className="p-2 font-semibold border outline-none">
               <option value="Order Placed">Order Placed</option>
               <option value="Packing">Packing</option>
               <option value="Shipped">Shipped</option>
