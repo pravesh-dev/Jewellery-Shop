279dbaa43be51fe2d45fdfd22354eceb1dc002df
pravesh dav <humantorch1932@gmail.com>
2025-06-23 13:29:23 +0530

    show hide pass

--- Frontend/src/Components/LoginSignupPages/Signup.jsx
+++ Frontend/src/Components/LoginSignupPages/Signup.jsx
@@ -1,5 +1,5 @@
 // Import necessary libraries and assets
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import axios from "axios"; // Import axios for HTTP requests
 import sideImage from "../../Assets/loginSignupPage/login-side-image.svg";
 import name from "../../Assets/loginSignupPage/name.svg";
@@ -8,12 +8,16 @@ import call from "../../Assets/loginSignupPage/call.svg";
 import lock from "../../Assets/loginSignupPage/lock.svg";
 import { Link, useNavigate } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
+import { IoIosEye } from "react-icons/io";
+import { IoIosEyeOff } from "react-icons/io";
 
 // Define the Signup component
 function Signup() {
   const navigate = useNavigate();
   const { token, setToken, backendUrl } = useContext(ShopContext);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const passRef = useRef(null)
 
   // Initialize state for form data and response message
   const [formData, setFormData] = useState({
@@ -34,6 +38,11 @@ function Signup() {
       [name]: value,
     });
   };
+
+  // Function to handle password visibility
+  const changePassVisiblity = () => {
+    setShowPassword(!showPassword);
+  }
 
   // Function to handle form submission
   const handleFormSubmit = async (e) => {
@@ -164,13 +173,17 @@ function Signup() {
               alt="lock icon"
             />
             <input
-              type="password"
+              ref={passRef}
+              type={showPassword ? 'text' : 'password'}
               name="password"
               value={formData.password}
               onChange={handleInputChange}
               placeholder="Password"
               className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16 xl:py-0 xl:h-full xl:text-[1.12rem] xl:pl-20"
             />
+            {
+              showPassword ? <IoIosEye className="text-2xl text-primary absolute top-1/2 right-3 xl:right-6 xl:text-3xl -translate-y-1/2" onClick={changePassVisiblity} /> : <IoIosEyeOff className="text-2xl text-primary absolute top-1/2 right-3 xl:right-6 xl:text-3xl -translate-y-1/2" onClick={changePassVisiblity} />
+            }
           </div>
           <div className="relative xl:w-[28.6rem] xl:h-[3.6rem]">
             <img
@@ -179,13 +192,16 @@ function Signup() {
               alt="lock icon"
             />
             <input
-              type="password"
+              type={showPassword ? 'text' : 'password'}
               name="confirmPassword"
               value={formData.confirmPassword}
               onChange={handleInputChange}
               placeholder="Re-Enter Password"
               className="w-full pl-12 py-3 rounded-md border-0 shadow-lg shadow-[#C8F3BF] focus:outline-none focus:ring-1 ring-primary lg:rounded-full lg:pl-16 xl:py-0 xl:h-full xl:text-[1.12rem] xl:pl-20"
             />
+            {
+              showPassword ? <IoIosEye className="text-2xl text-primary absolute top-1/2 right-3 xl:right-6 xl:text-3xl -translate-y-1/2" onClick={changePassVisiblity} /> : <IoIosEyeOff className="text-2xl text-primary absolute top-1/2 right-3 xl:right-6 xl:text-3xl -translate-y-1/2" onClick={changePassVisiblity} />
+            }
           </div>
         </div>
 
