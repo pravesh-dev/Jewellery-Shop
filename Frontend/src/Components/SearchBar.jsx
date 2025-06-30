655ade1114f3f826e972c73ddd62957c0fbe4749
pravesh dav <humantorch1932@gmail.com>
2025-06-30 18:38:24 +0530

    search bar only visible in shop page

--- Frontend/src/Components/SearchBar.jsx
+++ Frontend/src/Components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { ShopContext } from "../Context/ShopContext";
 import { IoMdSearch } from "react-icons/io";
 import { RiCloseFill } from "react-icons/ri";
@@ -10,14 +10,19 @@ const SearchBar = () => {
 
     const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
     const location = useLocation();
+    const [visible, setVisible] = useState(false);
 
     useEffect(()=>{
 
-      console.log(location)
+      if(location.pathname.includes('shop') && showSearch){
+        setVisible(true)
+      } else{
+        setVisible(false);
+      }
 
     }, [location])
 
-  return showSearch ? (
+  return showSearch && visible ? (
     <div className="pt-28 border-t border-b bg-accent text-center">
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 mx-3 my-5 rounded-full w-3/4 sm:w-1/2">
       <input className="flex-1 outline-none bg-inherit text-sm" value={search} onChange={(e)=> {setSearch(e.target.value)}} type="text" placeholder="Search" />
